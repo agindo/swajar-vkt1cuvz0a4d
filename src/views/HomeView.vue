55ed0c2dfddea975d3c4a9ce2454cf8fac5bf7c8
Agindo Ramadhan <ramadhanagindo@gmail.com>
2024-05-08 11:51:08 +0700

    ui login / register

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,50 @@
 <template>
   <div>
-    <h1>This is an home page</h1>
+<!-- component -->
+<!-- Main container -->
+<div>
+  <!-- Flex container for centering items -->
+  <div class="flex h-screen flex-col items-center justify-center">
+    <!-- Container for login form -->
+    <div class="max-h-auto mx-auto max-w-xl w-60">
+      <!-- Login title and description -->
+      <div class="mb-4">
+        <h1 class="text-xl font-semibold">Welcome back</h1>
+        <p class="text-gray-400 leading-tight text-sm">Welcome back! Please enter your account</p>
+      </div>
+      <!-- Login form -->
+      <div>
+        <el-input placeholder="Username" v-model="username"></el-input>
+      </div>
+      <div class="mt-1">
+        <el-input placeholder="Password" v-model="password" show-password></el-input>
+      </div>
+      <div class="mt-2">
+        <el-button type="primary" size="small" class="uppercase w-full">Login / Sign In</el-button>
+      </div>
+      <!-- Signup link -->
+      <p class="text-center text-xs mt-3 p-0">No account? <a class="text-blue-500 cursor-pointer" @click="getViews('/PBcw3g0kC0xt')">Create one</a> </p>
+    </div>
+  </div>
+</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  data() {
+    return {
+      username: null,
+      password: null
+    }
+  },
+  methods: {
+    getViews: function(value){
+      if (this.$route.path !== value) {
+        this.$router.push(value);
+      }
+    }
+  }
 }
 </script>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,11 +1,45 @@
 <template>
   <div>
-    <h1>This is an register page</h1>
+    <div>
+  <!-- Flex container for centering items -->
+  <div class="flex h-screen flex-col items-center justify-center">
+    <!-- Container for login form -->
+    <div class="max-h-auto mx-auto max-w-xl w-60">
+      <!-- Login title and description -->
+      <div class="mb-4">
+        <h1 class="text-xl font-semibold">Welcome back</h1>
+        <p class="text-gray-400 leading-tight text-sm">Welcome back! Please enter your account</p>
+      </div>
+      <!-- Login form -->
+      <div>
+        <el-input placeholder="Username" v-model="username"></el-input>
+      </div>
+      <div class="mt-2">
+        <el-button type="primary" size="small" class="uppercase w-full">Register / Sign Up</el-button>
+      </div>
+      <!-- Signup link -->
+      <p class="text-center text-xs mt-3 p-0">Have account? <a class="text-blue-500 cursor-pointer" @click="getViews('/')">Login</a> </p>
+    </div>
+  </div>
+</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'RegisterView'
+  name: 'RegisterView',
+  data() {
+    return {
+      username: null,
+      password: null
+    }
+  },
+  methods: {
+    getViews: function(value){
+      if (this.$route.path !== value) {
+        this.$router.push(value);
+      }
+    }
+  }
 }
 </script>
