f7ee6b362d175feb50c4a143f51f0c71ef63cefa
Agindo Ramadhan <ramadhanagindo@gmail.com>
2024-05-08 11:25:26 +0700

    all update

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,12 @@ import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 
+import HomeMediaView from '../views/media/HomeView.vue'
+import HomeSirelaView from '../views/sirela/HomeView.vue'
+import HomeProfileView from '../views/profile/HomeView.vue'
+
+import RegisterView from '../views/RegisterView.vue'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -15,6 +21,26 @@ const routes = [
     path: '/about',
     name: 'about',
     component: AboutView
+  },
+  {
+    path: '/F6D2wQo5g6Ka/z1s6QUD8D2Cx/:id',
+    name: 'HomeMediaView',
+    component: HomeMediaView
+  },
+  {
+    path: '/603m5d3M5kE6',
+    name: 'HomeSirelaView',
+    component: HomeSirelaView
+  },
+  {
+    path: '/tP4KJ16Q8wm2',
+    name: 'HomeProfileView',
+    component: HomeProfileView
+  },
+  {
+    path: '/PBcw3g0kC0xt',
+    name: 'RegisterView',
+    component: RegisterView
   }
 ]
 
