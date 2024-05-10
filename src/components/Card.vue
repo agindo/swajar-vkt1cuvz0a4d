414f7c58bbec99f84c76dd278e67175869febffb
Agindo Ramadhan <ramadhanagindo@gmail.com>
2024-05-10 19:49:28 +0700

    add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="card">
+      <!-- <p>{{ data }}</p> -->
+      <p class="p-0 m-0" :class="data.look.class">{{ data.title }}</p>
+      <p class="raleway-400 p-0 m-0">{{ data.description }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    data: Object
+  }
+}
+</script>
+<style scoped>
+.card {
+  padding: 2.5rem;
+  background-color: #FFF;
+  /* box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px; */
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div>
+  <div class="bg-gray-50">
     <div class="flex h-screen flex-col items-center justify-center">
       <div class="m-36">
+        
         <div class="flex flex-wrap">
           <div class="flex-auto m-1" v-for="(item, index) in menu" :key="index">
-            <el-card shadow="always" class="p-5">
+            <!-- <el-card shadow="hover" class="cursor-pointer p-5">
               <div>
                 <p class="p-0 m-0" :class="item.look.class">{{ item.title }}</p>
                 <p class="raleway-400 p-0 m-0">{{ item.description }}</p>
               </div>
-              <div class="mt-3">
-                <el-button icon="el-icon-right" size="mini" class="lato-regular">Lanjut</el-button>
-              </div>
-            </el-card>
+            </el-card> -->
+            <Card :data="item" />
           </div>
         </div>
       </div>
@@ -21,8 +20,12 @@
 </template>
 <script>
 import menu from '@/assets/json/ingredient.json'
+import Card from '../components/Card.vue'
 export default {
   name: 'AboutView',
+  components: {
+    Card
+  },
   data() {
     return {
       menu: menu.materi
